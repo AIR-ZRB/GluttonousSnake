592a45e1bbbbb4cb3f30a45d99569b9b28a69f3d
青空 <1057605824@qq.com>
2020-04-20 22:14:18 +0800

    主页完成

---
+++ src/components/slidingBlock/slidingBlock.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class slidingBlock extends React.Component { 
+
+    render(){
+        return (
+            <div>我是 sliding block 模块</div>
+        )
+    }
+} 
